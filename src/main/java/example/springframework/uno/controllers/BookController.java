c58cee9f732e4b4d25ad5c7d85b8c210f9bbd9d1
antoniofsiqueira <antonio.fsiqueira@gmail.com>
2022-07-30 05:44:22 -0300

    List of books

--- src/main/java/example/springframework/uno/controllers/BookController.java
+++ src/main/java/example/springframework/uno/controllers/BookController.java
@@ -1,4 +1,27 @@
 package example.springframework.uno.controllers;
 
+
+import example.springframework.uno.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+
+
 }
